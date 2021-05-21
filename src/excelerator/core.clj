f42e9e5c3e82456a61ec1c45d3a93beee6e31a7a
Daniel Heiniger <daniel.r.heiniger@gmail.com>
2021-05-20 20:56:42 -0500

    Update the format that is accepted

--- src/excelerator/core.clj
+++ src/excelerator/core.clj
@@ -2,28 +2,53 @@
   (:require [clojure.string :as str])
   (:use [seesaw.core]
         [seesaw.mig])
-
   (:gen-class :main true))
 
-(def user-input-text-area (text :id :input
-                                :multi-line? true
-                                :editable? true
-                                :text ""))
+(def background-color "#c8dce3")
 
-(def output-text-area (text :multi-line? true
-                            :editable? false
-                            :text ""))
+(defn format-string
+  [s]
+  (str/split s #"\n"))
+
+(defn format-strings
+  [s]
+  (if (not (str/includes? s "\""))
+    (format-string s)
+    (map #(str/split % #"\s+")
+         (map #(str/replace % "\n" "")
+              (remove str/blank?
+                      (str/split s #"\""))))))
+
+(defn total
+  "converts a collection of strings and turns each value into double"
+  [coll]
+  (cond
+    (coll? coll) (format "%.2f" (apply + (map #(Double/parseDouble %) coll)))
+    (str/blank? coll) ""
+    :else (format "%.2f" (Double/parseDouble coll))))
 
 (defn cell+
   [s]
-  (Double/parseDouble
-    (format "%.2f"
-            (apply +
-                   (map #(if (str/blank? %) 0.0
-                                            (Double/parseDouble %))
-                        (map str/trim (str/split-lines (str/replace s "\"" ""))))))))
+  (str/join "\n" (map total (format-strings s))))
 
-(defn swap-output-text []
+;;USER INTERFACE-------------------------
+(def user-input-text-area (text :id :input
+                                :multi-line? true
+                                :editable? true
+                                :text ""
+                                :background background-color
+                                :listen [:key-typed (fn [_] (config! _ :background "#ff9375"))]))
+
+(def output-text-area (text :multi-line? true
+                            :editable? false
+                            :background background-color
+                            :text ""))
+
+(defn swap-input-text-handler []
+  (let [i (select user-input-text-area [:#input])]
+    (config! i :background background-color)))
+
+(defn swap-output-text-handler []
   (let [i (value (select user-input-text-area [:#input]))]
     (prn i)
     (try
@@ -31,19 +56,24 @@
       (catch Exception _
         (text! output-text-area "INVALID VALUE")))))
 
-(def calculate-button (button :text "Calculate"
+(def calculate-button (button :id :calculate-button
+                              :text "Calculate"
                               :font "ARIAL-BOLD-14"
                               :border -1
-                              :background "#c8dce3"
+                              :background background-color
                               :mnemonic \newline
-                              :listen [:action (fn [_] (swap-output-text))]))
+                              :listen [:action (fn [_] (swap-input-text-handler))
+                                       :action (fn [_] (swap-output-text-handler))]))
 
+(def help-action (menu-item :text "About" :listen [:action (fn [_](alert "Version: 1.0\nAuthor: Daniel Heiniger"))]))
 
 (defn -main []
   (-> (frame :title "Excelerator"
              :width 600
              :height 800
              :on-close :dispose
+             :menubar (menubar :items
+                               [(menu :text "Help" :items [help-action])])
              :content (grid-panel
                         :columns 3
                         :items [(border-panel :north (flow-panel :vgap 20)
@@ -60,6 +90,8 @@
                                               :south (flow-panel :vgap 20)
                                               :east (flow-panel :hgap 10))]))
       show!))
+
+
 
 
 
--- test/excelerator/core_test.clj
+++ test/excelerator/core_test.clj
@@ -4,19 +4,19 @@
 
 (deftest empty-string
   (testing "An empty string"
-    (is (= 0.0 (cell+ "")))))
+    (is (= "" (cell+ "")))))
 
 (deftest simple-string
   (testing "A simple numeric string"
-    (is (= 1.0 (cell+ "1")))))
+    (is (= "1.00" (cell+ "1")))))
 
 (deftest string-with-many-numeric-values
   (testing "A string with many numbers, but no new lines"
-    (is (thrown? NumberFormatException (cell+ "1 2 3 4 5 6 7 8 9 10")))))
+    (is (thrown? NumberFormatException  (cell+ "1 2 3 4 5 6 7 8 9 10")))))
 
 (deftest string-with-multiple-lines
   (testing "A string with many numbers, each on its own line"
-    (is (= 55.0 (cell+ "1\n 2\n 3\n 4\n 5\n 6\n 7\n 8\n 9\n 10")))))
+    (is (= "1.00\n2.00\n3.00\n4.00\n5.00\n6.00" (cell+ "1\n 2\n 3\n 4\n 5\n 6")))))
 
 (deftest string-with-multiple-lines-with-non-numeric-values
   (testing "A string with many numbers and 1 non-numeric character, each on its own line"
@@ -24,8 +24,12 @@
 
 (deftest string-with-multiple-lines-with-1-blank-line
   (testing "A string with many numbers but a single blank line"
-    (is (= 55.0 (cell+ "1\n\n 2\n 3\n 4\n 5\n 6\n 7\n 8\n 9\n 10")))))
+    (is (= "1.00\n\n2.00\n3.00\n4.00\n5.00\n6.00" (cell+ "1\n\n 2\n 3\n 4\n 5\n 6")))))
 
 (deftest string-with-multiple-lines-with-2-blank-lines
   (testing "A string with many numbers but a single blank line"
-    (is (= 55.0 (cell+ "1\n\n\n 2\n 3\n 4\n 5\n 6\n 7\n 8\n 9\n 10")))))
+    (is (= "1.00\n\n\n2.00\n3.00\n4.00\n5.00\n6.00" (cell+ "1\n\n\n 2\n 3\n 4\n 5\n 6\n")))))
+
+(deftest multiple-strings-delimited-by-quotes
+  (testing "A string that contains multiple strings, delimited by \""
+    (is (= "286.47\n85.00\n16.00\n79.05\n24.00\n16.00\n317.02\n28.95\n440.15" (cell+ "\"7.60   \n64.00   \n24.00   \n0.00   \n0.00   \n0.00   \n79.18   \n74.24   \n0.00   \n37.45\"\n\"50.00   \n0.00   \n0.00   \n35.00\"\n16.00\n\"10.00   \n9.10   \n59.95\"\n\"24.00   \n0.00\"\n16.00\n\"9.10   \n50.00   \n51.10   \n34.95   \n25.00   \n86.87   \n15.00   \n45.00\"\n\"9.95   \n0.00   \n19.00\"\n\"100.00   \n20.00   \n60.00   \n0.00   \n25.00   \n65.00   \n60.00   \n25.00   \n33.15   \n15.00   \n25.00   \n12.00   \n0.00\"")))))
